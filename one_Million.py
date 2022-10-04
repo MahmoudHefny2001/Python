1acc6edcf6258a40926813e82f2eaa7da8437023
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-04 18:11:55 +0200

    message

---
+++ one_Million.py
@@ -0,0 +1,7 @@
+numbers = list(range(1, 1000001))
+#for i in numbers:
+   # print(i)
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
---
+++ threes.py
@@ -0,0 +1,6 @@
+three_multiplies = []
+for number in range(3, 31):
+    three_multiplies.append(number * 3)
+
+for number in three_multiplies:
+    print(number)

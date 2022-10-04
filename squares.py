ad7f8b5dba44eb8f24d0a0e095de946d3d58f6dd
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-04 03:01:09 +0200

    Message

---
+++ squares.py
@@ -0,0 +1,14 @@
+# squares = []
+
+# for number in range(1, 11):
+    # number_squared = number ** 2
+    # squares.append(number_squared)
+
+# print(squares)
+
+squares = []
+
+for number in range(1, 11):
+    squares.append(number ** 2)
+
+print(squares)

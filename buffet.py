158e42d6c4c0b838d17816a56ed2791c9fefddf1
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-05 17:10:25 +0200

    message

---
+++ buffet.py
@@ -0,0 +1,8 @@
+basic_foods = ('Pizza', 'Hamburger', 'Falafel', 'Chicken', 'Pasta')
+for food in basic_foods:
+    print(food)
+print()
+basic_foods = ('Pizza', 'Indian Food', 'Falafel', 'Chicken', 'mexican Food')
+for food in basic_foods:
+    print(food)
+

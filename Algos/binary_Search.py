fca37f410d1227fbe471165287ba231ba44c22ee
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-05 02:43:23 +0200

    message

---
+++ Algos/binary_Search.py
@@ -0,0 +1,22 @@
+def binary_search(array, item):
+    low = 0
+    high = len(array)-1
+    while low <= high:
+        mid = int((low+high)/2)
+        guess = array[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid -1
+        else:
+            low = mid + 1
+    return None
+
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+item = int(input("Choose a number to get its index: "))
+position = binary_search(numbers, item)
+print("Position of "+str(item)+" is: "+ str(position))
+

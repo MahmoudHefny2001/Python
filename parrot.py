0dda5bad601388e34623de308516b065730ca0a0
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-08 19:25:44 +0200

    basics

--- parrot.py
+++ parrot.py
@@ -1,2 +1,19 @@
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+#message = input("Tell me something, and I will repeat it back to you: ")
+#print(message)
+
+prompt = "\nTell me something, and I will repeat it back to you or enter 'quit' to end the program: "
+#prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+#while message != 'quit':
+    #message = input(prompt).lower()
+    #if message != 'quit':
+        #print(message.title())
+
+go = True
+while go :
+    message = input(prompt)
+    if message.lower() == 'quit':
+        go = False
+    else:
+        print(message)
